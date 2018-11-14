50d21781a642857071fcb684d34e15e94cc2fbbf
Juan <ortizcoj@my.erau.edu>
2018-11-14 11:15:00 -0500

    Added campus image

--- app/src/main/java/com/example/juanc/parkinglotdemo4/MainActivity.java
+++ app/src/main/java/com/example/juanc/parkinglotdemo4/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ImageView mImageView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,10 +22,12 @@ public class MainActivity extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_ride);
+                    mTextMessage.setTextSize(20);
+                    mImageView.setVisibility(View.INVISIBLE);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_map);
-                    mTextMessage.setTextSize(40);
+                    mTextMessage.setVisibility(View.GONE);
+                    mImageView.setVisibility(View.VISIBLE);
                     return true;
             }
             return false;
@@ -35,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextMessage = findViewById(R.id.message);
+        mImageView = findViewById(R.id.map);
+        mImageView.setVisibility(View.INVISIBLE);
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
